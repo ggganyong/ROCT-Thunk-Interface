9e5cbecaf28d9d204b6080d55724055d8ea3d093
Philip Yang <Philip.Yang@amd.com>
2019-12-17 14:53:39 -0500

    kfdtest: add KFDSVMRangeTest BasicSystemMemTest
    Register svm ranges with prefetch location on CPU, use shader to copy
    content to verify GPU page table mapping is correct.
    
    Change-Id: If6f68f54bb4298f97db8fae119652c972a4d45a4
    Signed-off-by: Philip Yang <Philip.Yang@amd.com>

---
+++ tests/kfdtest/src/KFDSVMRangeTest.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2020 Advanced Micro Devices, Inc. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "KFDSVMRangeTest.hpp"
+#include "PM4Queue.hpp"
+#include "PM4Packet.hpp"
+#include "SDMAPacket.hpp"
+#include "SDMAQueue.hpp"
+#include "Dispatch.hpp"
+
+void KFDSVMRangeTest::SetUp() {
+    ROUTINE_START
+
+    KFDBaseComponentTest::SetUp();
+
+    m_pIsaGen = IsaGenerator::Create(m_FamilyId);
+
+    ROUTINE_END
+}
+
+void KFDSVMRangeTest::TearDown() {
+    ROUTINE_START
+
+    if (m_pIsaGen)
+        delete m_pIsaGen;
+    m_pIsaGen = NULL;
+
+    KFDBaseComponentTest::TearDown();
+
+    ROUTINE_END
+}
+
+TEST_F(KFDSVMRangeTest, BasicSystemMemTest) {
+    TEST_REQUIRE_ENV_CAPABILITIES(ENVCAPS_64BITLINUX);
+    TEST_START(TESTPROFILE_RUNALL);
+
+    PM4Queue queue;
+    HSAuint64 AlternateVAGPU;
+    unsigned int BufferSize = PAGE_SIZE;
+
+    int defaultGPUNode = m_NodeInfo.HsaDefaultGPUNode();
+    ASSERT_GE(defaultGPUNode, 0) << "failed to get default GPU Node";
+
+    if (!GetVramSize(defaultGPUNode)) {
+        LOG() << "Skipping test: No VRAM found." << std::endl;
+        return;
+    }
+
+    HsaMemoryBuffer isaBuffer(PAGE_SIZE, defaultGPUNode);
+    HsaSVMRange srcSysBuffer(BufferSize, defaultGPUNode);
+    HsaSVMRange destSysBuffer(BufferSize, defaultGPUNode);
+
+    srcSysBuffer.Fill(0x01010101);
+
+    m_pIsaGen->GetCopyDwordIsa(isaBuffer);
+
+    ASSERT_SUCCESS(queue.Create(defaultGPUNode));
+    queue.SetSkipWaitConsump(0);
+
+    Dispatch dispatch(isaBuffer);
+
+    dispatch.SetArgs(srcSysBuffer.As<void*>(), destSysBuffer.As<void*>());
+    dispatch.Submit(queue);
+    dispatch.Sync(g_TestTimeOut);
+
+    EXPECT_SUCCESS(queue.Destroy());
+
+    EXPECT_EQ(destSysBuffer.As<unsigned int*>()[0], 0x01010101);
+
+    TEST_END
+}
